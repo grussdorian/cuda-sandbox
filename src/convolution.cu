8df69b57bb8743c8e622075f740cb5c443ee418c
Gruss Dorian <45635468+grussdorian@users.noreply.github.com>
2022-04-04 21:25:28 +0530

    working convolution 1D x 2
    Convolution using two 1D masks cascaded one after another

--- src/convolution.cu
+++ src/convolution.cu
@@ -4,6 +4,7 @@
 #include <math.h>
 
 __global__ void convolution(float *mat_input1, float *mat_conv_input1, float *mat_output1, int mat_datasize, int mat_dim);
+__global__ void convolution2(float *mat_input1, float *mat_conv_input1, float *mat_output1, int mat_datasize, int mat_dim);
 
 __device__
 int getGlobalIdx_3D_3D(){
@@ -13,62 +14,151 @@ int getGlobalIdx_3D_3D(){
     return threadId;
 }
 
+// VERTICAL MASK
 __global__ void
-convolution(float *mat_input1, float *mat_conv_input1, float *mat_output1, int mat_datasize, int mat_dim)
+convolution2(float *mat_input1, float *mat_conv_input1, float *mat_output1, int mat_datasize, int mat_dim)
 {
-    //  int blockNum = blockIdx.x;
-    //  int threadNum = threadIdx.x;
-    //  int globalThreadId = blockNum*(blockDim.x) + threadNum;
 
-    //  int row_val = globalThreadId/mat_dim;
-    //  int col_val = globalThreadId%mat_dim;
+    mat_conv_input1[4] =0;
 
     int globalThreadId = getGlobalIdx_3D_3D();
 
+    int row_val = globalThreadId/mat_dim;
+    int col_val = globalThreadId%mat_dim;
 
-    // if(row_val<mat_dim && col_val<mat_dim)
-    if(globalThreadId < mat_datasize/sizeof(float))
+    if(row_val<mat_dim && col_val<mat_dim)
     {
-        if(globalThreadId % mat_dim ==0 ){
-            mat_output1[globalThreadId] = mat_input1[globalThreadId] * mat_conv_input1[4] + mat_input1[globalThreadId - mat_dim] * mat_conv_input1[1] +mat_input1[globalThreadId + mat_dim] * mat_conv_input1[7] +  mat_input1[globalThreadId+1] * mat_conv_input1[5];
-        }else if(globalThreadId > (mat_dim-1)*mat_dim){
-            mat_output1[globalThreadId] = mat_input1[globalThreadId- mat_dim - 1] * mat_conv_input1[0] + mat_input1[globalThreadId-1] * mat_conv_input1[3] +
-            mat_input1[globalThreadId] * mat_conv_input1[4] +
-            mat_input1[globalThreadId+1] * mat_conv_input1[5];
+    int index = mat_dim*row_val + col_val;
+	//Write code for convolution
+
+    // corner points
+
+        //top left corner
+        if(globalThreadId == 0 ){
+            mat_output1[globalThreadId] += mat_input1[globalThreadId] * mat_conv_input1[4]+  (mat_input1[mat_dim*(row_val+1) + col_val]*mat_conv_input1[7]);
         }
-        else if(globalThreadId==0){
-            mat_output1[globalThreadId] = mat_input1[globalThreadId] * mat_conv_input1[4] +  mat_input1[globalThreadId+1] * mat_conv_input1[5] + mat_input1[globalThreadId + mat_dim] * mat_conv_input1[7];
-        }else if( globalThreadId % (mat_dim-1) ==0){
-            mat_output1[globalThreadId] = mat_input1[globalThreadId] * mat_conv_input1[4] + mat_input1[globalThreadId - mat_dim] * mat_conv_input1[1] +mat_input1[globalThreadId + mat_dim] * mat_conv_input1[7]+mat_input1[globalThreadId-1] * mat_conv_input1[3];
-        }else{
-        float val = mat_input1[globalThreadId- mat_dim - 1] * mat_conv_input1[0] +
-                    mat_input1[globalThreadId - mat_dim] * mat_conv_input1[1] +
-                    mat_input1[globalThreadId - mat_dim +1] * mat_conv_input1[2] +
-
-                    mat_input1[globalThreadId-1] * mat_conv_input1[3] +
-                    mat_input1[globalThreadId] * mat_conv_input1[4] +
-                    mat_input1[globalThreadId+1] * mat_conv_input1[5] +
-
-                    mat_input1[globalThreadId + mat_dim -1] * mat_conv_input1[6] +
-                    mat_input1[globalThreadId + mat_dim] * mat_conv_input1[7] +
-                    mat_input1[globalThreadId + mat_dim +1]* mat_conv_input1[8];
-
-        mat_output1[globalThreadId] = val;
-        //mat_output1[index] = mat_input1[row_val*mat_dim + col_val];
+        //bottom left corner
+        else if(globalThreadId == mat_dim*(mat_dim -1 ) ) {
+            mat_output1[globalThreadId] = (mat_input1[index] * mat_conv_input1[4])
+                +  (mat_input1[mat_dim*(row_val-1) + col_val]*mat_conv_input1[1]);
         }
+        //bottom right corner
+        else if(globalThreadId == mat_dim*mat_dim - 1 ) {
+            mat_output1[globalThreadId] += (mat_input1[index] * mat_conv_input1[4])
+                +  (mat_input1[mat_dim*(row_val-1) + col_val]*mat_conv_input1[1]);
+        }
+        //edge points
+
+        //leftmost column
+        else if( col_val == 0){
+            mat_output1[globalThreadId] += (mat_input1[index] * mat_conv_input1[4])
+                +  (mat_input1[mat_dim*(row_val-1) + col_val]*mat_conv_input1[1])
+                +  (mat_input1[mat_dim*(row_val+1) + col_val]*mat_conv_input1[7]);
+        }
+        //top row
+        else if(globalThreadId < mat_dim-1 && globalThreadId!=0 ){
+            mat_output1[globalThreadId] += (mat_input1[index] * mat_conv_input1[4])
+                +  (mat_input1[mat_dim*(row_val+1) + col_val]*mat_conv_input1[7]);
+        }
+        //rightmost column
+        else if(col_val == mat_dim-1 && globalThreadId != mat_dim-1 ){
+            mat_output1[globalThreadId] += (mat_input1[index] * mat_conv_input1[4])
+                +  (mat_input1[mat_dim*(row_val-1) + col_val]*mat_conv_input1[1])
+                +  (mat_input1[mat_dim*(row_val+1) + col_val]*mat_conv_input1[7]);
+        }
+        //top right corner
+        else if(globalThreadId == mat_dim-1){
+            mat_output1[globalThreadId] += (mat_input1[index] * mat_conv_input1[4]) + (mat_input1[mat_dim*(row_val+1) + col_val]*mat_conv_input1[7]);
+        }
+        //bottom row
+        else if(globalThreadId > mat_dim*(mat_dim-1) && globalThreadId < ((mat_dim*mat_dim)-1) ){
+             mat_output1[globalThreadId] +=  (mat_input1[index] * mat_conv_input1[4])
+                +  (mat_input1[mat_dim*(row_val-1) + col_val]*mat_conv_input1[1]);
+        } else{
+                mat_output1[index] += (mat_input1[index] * mat_conv_input1[4])
+                +  (mat_input1[mat_dim*(row_val-1) + col_val]*mat_conv_input1[1])
+                +  (mat_input1[mat_dim*(row_val+1) + col_val]*mat_conv_input1[7]);
+        }
+
     }
+
+}
+
+// HORIZONTAL MASK 
+__global__ void
+convolution(float *mat_input1, float *mat_conv_input1, float *mat_output1, int mat_datasize, int mat_dim)
+{
+    int globalThreadId = getGlobalIdx_3D_3D();
+
+    int row_val = globalThreadId/mat_dim;
+    int col_val = globalThreadId%mat_dim;
+
+    if(row_val<mat_dim && col_val<mat_dim)
+    {
+    int index = mat_dim*row_val + col_val;
+	//Write code for convolution
+
+    // corner points
+
+        //top left corner
+        if(globalThreadId == 0 ){
+            mat_output1[globalThreadId] = mat_input1[globalThreadId] * mat_conv_input1[4] + (mat_input1[index +1 ]*mat_conv_input1[5]);
+        }
+        //bottom left corner
+        else if(globalThreadId == mat_dim*(mat_dim -1 ) ) {
+            mat_output1[globalThreadId] = (mat_input1[index] * mat_conv_input1[4])
+                +  (mat_input1[index + 1 ]*mat_conv_input1[5]);
+        }
+        //bottom right corner
+        else if(globalThreadId == mat_dim*mat_dim - 1 ) {
+            mat_output1[globalThreadId] = (mat_input1[index] * mat_conv_input1[4])
+                 +(mat_input1[index-1]*mat_conv_input1[3]);
+        }
+        //edge points
+
+        //leftmost column
+        else if( col_val == 0){
+            mat_output1[globalThreadId] = (mat_input1[index] * mat_conv_input1[4])
+                +  (mat_input1[index +1 ]*mat_conv_input1[5]);
+        }
+        //top row
+        else if(globalThreadId < mat_dim-1 && globalThreadId!=0 ){
+            mat_output1[globalThreadId] = (mat_input1[index] * mat_conv_input1[4])
+                +  (mat_input1[index -1 ]*mat_conv_input1[3])
+                +  (mat_input1[index +1 ]*mat_conv_input1[5]);
+        }
+        //rightmost column
+        else if(col_val == mat_dim-1 && globalThreadId != mat_dim-1 ){
+            mat_output1[globalThreadId] =(mat_input1[index] * mat_conv_input1[4])
+                +  (mat_input1[index -1 ]*mat_conv_input1[3]);
+        }
+        //top right corner
+        else if(globalThreadId == mat_dim-1){
+            mat_output1[globalThreadId] = (mat_input1[index] * mat_conv_input1[4]) +  (mat_input1[index -1 ]*mat_conv_input1[3]);
+        }
+        //bottom row
+        else if(globalThreadId > mat_dim*(mat_dim-1) && globalThreadId < ((mat_dim*mat_dim)-1) ){
+             mat_output1[globalThreadId] =  (mat_input1[index] * mat_conv_input1[4])
+                +  (mat_input1[index -1 ]*mat_conv_input1[3])
+                +  (mat_input1[index +1 ]*mat_conv_input1[5]);;
+        } else{
+                mat_output1[index] = (mat_input1[index] * mat_conv_input1[4])
+                +  (mat_input1[index -1 ]*mat_conv_input1[3])
+                +  (mat_input1[index +1 ]*mat_conv_input1[5]);
+        }
+
+    }
+
 }
 
 void print_matrix(float *A,int m,int n)
 {
-   // m = 10; n =512;
     for(int i =0;i<m;i++)
     {
         for(int j=0;j<n;j++)
             printf("%.2f ",A[i*m+j]);
         printf("\n");
     }
-    //printf("%.2f ",A[512]);
 }
 int main(void)
 {
@@ -123,7 +213,6 @@ int main(void)
             fprintf(stderr, "Failed to allocate device vector d_mat_conv_input (error code %s)!\n", cudaGetErrorString(err));
             exit(EXIT_FAILURE);
         }
-
         float *d_mat_output1 = NULL;
         err = cudaMalloc((void **)&d_mat_output1, mat_size);
         if (err != cudaSuccess)
@@ -132,7 +221,6 @@ int main(void)
             exit(EXIT_FAILURE);
         }
 
-       
         err = cudaMemcpy(d_mat_input1, h_mat_input1, mat_size, cudaMemcpyHostToDevice);
         if (err != cudaSuccess)
         {
@@ -151,8 +239,9 @@ int main(void)
         dim3 mat_grid_dim(gx,gy,4);
         // max threads per block = 1024 for backwards compatibility
         dim3 mat_block_dim(32,32,1);
+        //successive use of 1D masks
         convolution<<<mat_grid_dim, mat_block_dim>>>(d_mat_input1, d_mat_conv_input, d_mat_output1, mat_size, mat_dim);
-        
+        convolution2<<<mat_grid_dim, mat_block_dim>>>(d_mat_input1, d_mat_conv_input, d_mat_output1, mat_size, mat_dim);
 
        
         err = cudaGetLastError();
@@ -189,7 +278,6 @@ int main(void)
             fprintf(stderr, "Failed to free device vector d_mat_output1 (error code %s)!\n", cudaGetErrorString(err));
             exit(EXIT_FAILURE);
         }
-        
        print_matrix(h_mat_output1,mat_dim,mat_dim);
         
         free(h_mat_input1);
@@ -201,7 +289,6 @@ int main(void)
             fprintf(stderr, "Failed to deinitialize the device! error=%s\n", cudaGetErrorString(err));
             exit(EXIT_FAILURE);
         }
-        // printf("gx = %d gy= %d \n", gx,gy);
     }
     return 0;
 }
